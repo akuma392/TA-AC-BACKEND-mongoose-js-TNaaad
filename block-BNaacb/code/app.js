9e4ee410342e320b12b0be679eb9425bcd173e7e
Abhishek Kumar <abhivision3@gmail.com>
2021-03-05 20:23:37 +0530

    refactor

--- block-BNaacb/code/app.js
+++ block-BNaacb/code/app.js
@@ -2,10 +2,6 @@ var express = require('express');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var userSchema = new Schema({
-  name: String,
-  age: Number,
-});
 
 mongoose.connect(
   'mongodb://localhost/sample',
