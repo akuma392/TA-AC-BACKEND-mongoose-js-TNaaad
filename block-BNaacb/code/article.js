a9c76971faa8d3b2cb72159f420e51bb7371b806
Abhishek Kumar <abhivision3@gmail.com>
2021-03-05 20:04:41 +0530

    first commit

---
+++ block-BNaacb/code/article.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+  name: String,
+  age: Number,
+});
+
+mongoose.connect(
+  'mongodb://localhost/sample',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log(err ? err : 'Connected to database');
+  }
+);
+var app = express();
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+app.listen(4009, () => {
+  console.log('server is running at 4009');
+});
