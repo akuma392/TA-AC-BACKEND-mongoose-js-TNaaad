e7e6c7460839fbec1f4c33a7a337a65e5ee9a059
Abhishek Kumar <abhivision3@gmail.com>
2021-03-07 12:22:54 +0530

    user schema

---
+++ block-BNaacn/code/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+mongoose.connect(
+  'mongodb://localhost/sample',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log(err ? err : 'Connected to database');
+  }
+);
+var app = express();
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+app.listen(4009, () => {
+  console.log('server is running at 4009');
+});
